39e32e1ba24174de8b0b3e51f60e08ae9bbf55b0
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2019-12-20 15:09:00 +0200

    commit A

--- WebApplication1/WebApplication1/Models/a.cs
+++ WebApplication1/WebApplication1/Models/a.cs
@@ -8,5 +8,7 @@ namespace WebApplication1.Models
     public class a
     {
         public string propertyA { get; set; }
+        public string propertyB { get; set; }
+        public string propertyC { get; set; }
     }
 }
